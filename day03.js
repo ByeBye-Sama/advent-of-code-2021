9baf57dbac4a433d3380374a438c2aa80d8e8ccc
ByeBye-Sama <joaquin.byebye@gmail.com>
2021-12-10 23:08:01 -0500

    feat: third day

--- day03.js
+++ day03.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const array = fs
-  .readFileSync("day3.txt", { encoding: "utf-8" })
+  .readFileSync("day03.txt", { encoding: "utf-8" })
   .split("\n")
   .filter((x) => Boolean(x));
 
