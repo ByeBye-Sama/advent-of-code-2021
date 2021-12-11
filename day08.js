91acc2693b29cd6f5107f824b3d6674c953aa2e2
ByeBye-Sama <joaquin.byebye@gmail.com>
2021-12-10 23:16:03 -0500

    feat: eighth day

--- day08.js
+++ day08.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const array = fs
-  .readFileSync("day8.txt", { encoding: "utf-8" })
+  .readFileSync("day08.txt", { encoding: "utf-8" })
   .split("\n")
   .filter((x) => Boolean(x))
   .map((line) => {
